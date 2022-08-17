22cbe62ca6dc4a35f2e9101596fb81e82e4b6f21
Sayuck <nobrega.m.roberto@gmail.com>
2022-08-16 23:01:45 -0300

    Adiciona modelagem do banco e sua model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from routers import city, workstation
 
 app = FastAPI()
 
---
+++ src/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+
+
+class Workstation(Base):
+    __tablename__ = "workstation"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    adsl_vpn = Column(Boolean, nullable=False)
+    link = Column(String(250), nullable=True)
+    ip = Column(String(250), nullable=True)
+    regional = Column(Boolean, nullable=False, default=False)
+    city_id = Column(Integer, ForeignKey("city.id"), nullable=False)
+    regional_id = Column(Integer, ForeignKey("workstation.id"), nullable=True)
+
+
+phone = Table(
+    "phone",
+    Base.metadata,
+    Column("id_workstation", Integer, ForeignKey("workstation.id")),
+    Column("phone", String(250), nullable=False),
+)
+
