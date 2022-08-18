f1e3f27911456839a18c7feed35691947db6cb04
davi matias <69313657+daves22@users.noreply.github.com>
2022-08-17 22:17:05 -0300

    feat: criação do endpoint de busca de cidade

--- src/routers/city.py
+++ src/routers/city.py
@@ -26,6 +26,14 @@ class CityModel(BaseModel):
 Base.metadata.create_all(bind=engine)
 
 
+def get_error_response(e: Exception):
+    return {
+        "message": "Erro ao processar dados",
+        "error": str(e),
+        "data": None,
+    }
+
+
 @router.post("/city", tags=["City"], response_model=CityModel)
 async def post_city(data: CityModel, db: Session = Depends(get_db)):
     try:
@@ -57,5 +65,63 @@ async def post_city(data: CityModel, db: Session = Depends(get_db)):
 
         return JSONResponse(
             content=response_data,
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+@router.get("/city", tags=["City"])
+async def get_city(
+    city_id: Union[int, None] = None,
+    db: Session = Depends(get_db),
+    workstation_id: Union[int, None] = None,
+):
+    try:
+        if city_id:
+            city = db.query(City).filter_by(id=city_id).one_or_none()
+            if city:
+                city = jsonable_encoder(city)
+                message = "Dados buscados com exito"
+                status_code = status.HTTP_200_OK
+            else:
+                message = "Nenhuma cidade encontrada"
+                status_code = status.HTTP_200_OK
+
+            response_data = {
+                "message": message,
+                "error": None,
+                "data": city,
+            }
+            return JSONResponse(
+                content=jsonable_encoder(response_data),
+                status_code=status_code,
+            )
+
+        else:
+            if workstation_id:
+                all_data = (
+                    db.query(City)
+                    .filter_by(workstation_id=workstation_id, active=True)
+                    .all()
+                )
+            else:
+                all_data = db.query(City).filter_by(active=True).all()
+            all_data = [jsonable_encoder(c) for c in all_data]
+
+            response_data = {
+                "message": "Dados buscados com sucesso",
+                "error": None,
+                "data": all_data,
+            }
+            return JSONResponse(
+                content=response_data, status_code=status.HTTP_200_OK
+            )
+    except Exception as e:
+        response_data = {
+            "message": "Erro ao buscar dados",
+            "error": str(e),
+            "data": None,
+        }
+        return JSONResponse(
+            content=get_error_response(e),
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
