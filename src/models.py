5a3adf3c6a8a372c63731d2a8e2ee045e177dadb
Nicolas <nszchagas@gmail.com>
2022-08-17 22:37:48 -0300

    fiz: typo in asdl_vpn column.

--- src/models.py
+++ src/models.py
@@ -12,7 +12,7 @@ class Workstation(Base):
     __tablename__ = "workstation"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False, unique=True)
-    adsl_vpn = Column(Boolean, nullable=False)
+    asdl_vpn = Column(Boolean, nullable=False)
     link = Column(String(250), nullable=True)
     ip = Column(String(250), nullable=True)
     regional = Column(Boolean, nullable=False, default=False)
