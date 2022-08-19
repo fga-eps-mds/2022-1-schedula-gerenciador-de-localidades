901456e3b5179f9bb5aa1683352cccb23e3d9143
davi matias <69313657+daves22@users.noreply.github.com>
2022-08-19 09:02:34 -0300

    feat: criação do banco de testes de cidade e o teste de busca.py

---
+++ tests/test_get_city.py
@@ -0,0 +1,18 @@
+def test_city(client):
+    response = client.get("/city")
+    assert response.status_code == 200
+
+
+def test_city_id(client):
+    response = client.get("/city?city_id=2")
+    assert response.status_code == 200
+    assert response.json()["data"] == {
+        "id": 2,
+        "name": "cidade 2",
+    }
+
+
+def test_city_id_not_found(client):
+    response = client.get("/city?city_id=12")
+    assert response.status_code == 200
+    assert response.json()["message"] == "Nenhuma cidade encontrada"
