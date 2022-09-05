d52bef2947811b54fdbc119e32e9f588844122a1
davi matias <69313657+daves22@users.noreply.github.com>
2022-09-04 21:51:22 -0300

    criação do midleware

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers import city, workstation
 
 app = FastAPI()
-
-app.include_router(workstation.router)
-app.include_router(city.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,6 +14,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def process_request_headers(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process time"] = str(process_time)
+    return response
+
+app.include_router(workstation.router)
+app.include_router(city.router)
 
 @app.get("/")
 def root():
