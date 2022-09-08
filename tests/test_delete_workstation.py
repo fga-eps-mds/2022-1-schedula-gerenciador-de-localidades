2b9cec82894b320b99ecd8c0bd98cbc3a66eed11
davi matias <69313657+daves22@users.noreply.github.com>
2022-09-07 23:32:09 -0300

    ciração parametros para os testes de delete workstation

--- tests/test_delete_workstation.py
+++ tests/test_delete_workstation.py
@@ -1,5 +1,8 @@
-def test_delete_workstation(client):
-    response = client.delete("/workstation/1")
+from fastapi.testclient import TestClient
+from utils.auth_utils import ADMIN_HEADER, BASIC_HEADER, MANAGER_HEADER
+
+def test_delete_workstation_as_admin(client: TestClient):
+    response = client.delete("/workstation/1", headers=ADMIN_HEADER)
     assert response.status_code == 200
     assert (
         response.json()["message"] == "Posto de trabalho removido com sucesso"
@@ -9,7 +12,22 @@ def test_delete_workstation(client):
     # assert verify.json()["data"]["active"] == False
 
 
-def test_delete_non_existing_workstation(client):
-    response = client.delete("/workstation/90")
+def test_delete_non_existing_workstation_as_admin(client: TestClient):
+    response = client.delete("/workstation/90", headers=ADMIN_HEADER)
     assert response.status_code == 400
     assert response.json()["message"] == "Posto de trabalho não encontrado."
+
+def test_delete_workstation_as_manager(client: TestClient):
+    response = client.delete("/workstation/1", headers=MANAGER_HEADER)
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
+
+def test_delete_workstation_as_basic(client: TestClient):
+    response = client.delete("/workstation/1", headers=BASIC_HEADER)
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
+
+def test_delete_workstation_as_public(client: TestClient):
+    response = client.delete("/workstation/1")
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
