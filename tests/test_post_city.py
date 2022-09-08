0186b70dcebe6d34c03f2be340a9b896aeb98718
davi matias <69313657+daves22@users.noreply.github.com>
2022-09-07 23:58:03 -0300

    adicionei codigo para testes de 'post city'

--- tests/test_post_city.py
+++ tests/test_post_city.py
@@ -1,8 +1,51 @@
-def test_post_city(client):
-    response = client.post("/city", json={"name": "test"})
+from email.headerregistry import HeaderRegistry
+from wsgiref import headers
+from fastapi.testclient import TestClient
+from utils.auth_utils import ADMIN_HEADER, BASIC_HEADER, MANAGER_HEADER
+
+def test_post_city_as_admin(client:TestClient):
+    response = client.post(
+        "/city",json={"name": "test"},
+        headers=ADMIN_HEADER)
     assert response.status_code == 201
 
 
-def test_post_city_invalid_name(client):
-    response = client.post("/city", json={"name": None})
+def test_post_city_invalid_name_as_admin(client:TestClient):
+    response = client.post(
+        "/city",
+        json={"name": None},
+        headers=ADMIN_HEADER)
     assert response.status_code == 422
+
+
+def test_post_city_as_manager(client: TestClient):
+    response = client.post(
+        "/city",
+        json={"name": "test"},
+        headers=MANAGER_HEADER)
+    assert response.status_code == 201
+
+def test_post_city_invalid_name_as_manager(client:TestClient):
+    response = client.post(
+        "/city",
+        json={"name": None},
+        headers=MANAGER_HEADER)
+    assert response.status_code == 422
+
+def test_post_city_as_basic(client: TestClient):
+    response = client.post(
+        "/city",
+        json={"name":None},
+        headers=BASIC_HEADER
+    )
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"
+
+def test_post_city_as_public(client: TestClient):
+    response = client.post(
+        "/city",
+        json={"name":None},
+        headers=BASIC_HEADER
+    )
+    assert response.status_code == 401
+    assert response.json()["message"] == "Acesso negado"    
