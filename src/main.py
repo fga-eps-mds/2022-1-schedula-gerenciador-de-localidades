92374de99104d6e8d2e2828714480ef403178fa7
matheus <matheuscoding@gmail.com>
2022-09-28 20:45:10 -0300

    feat: Adiciona nova variavel de ambiente para conficao do cors

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -6,20 +8,23 @@ from routers import city, workstation
 from utils.auth_utils import get_authorization
 
 app = FastAPI()
-
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-response_unauthorized = JSONResponse({
-    "message": "Acesso negado",
-    "error": True,
-    "data": None,
-}, status.HTTP_401_UNAUTHORIZED)
+response_unauthorized = JSONResponse(
+    {
+        "message": "Acesso negado",
+        "error": True,
+        "data": None,
+    },
+    status.HTTP_401_UNAUTHORIZED,
+)
 
 
 @app.middleware("http")
@@ -28,24 +33,25 @@ async def process_request_headers(request: Request, call_next):
     method = str(request.method)
     url = str(request.url)
 
-    if method == 'DELETE':
-        if 'workstation' in url or 'city' in url:
-            if auth != 'admin':
+    if method == "DELETE":
+        if "workstation" in url or "city" in url:
+            if auth != "admin":
                 return response_unauthorized
-    if method == 'PUT':
-        if 'workstation' in url or 'city' in url:
-            if auth not in ['admin', 'manager']:
+    if method == "PUT":
+        if "workstation" in url or "city" in url:
+            if auth not in ["admin", "manager"]:
                 return response_unauthorized
-    if method == 'POST':
-        if 'workstation' in url or 'city' in url:
-            if auth not in ['admin', 'manager']:
+    if method == "POST":
+        if "workstation" in url or "city" in url:
+            if auth not in ["admin", "manager"]:
                 return response_unauthorized
-    if method == 'GET':
-        if 'workstation' in url or 'city' in url:
-            if auth not in ['admin', 'manager', 'basic', 'public']:
+    if method == "GET":
+        if "workstation" in url or "city" in url:
+            if auth not in ["admin", "manager", "basic", "public"]:
                 return response_unauthorized
 
     return await call_next(request)
+
 
 app.include_router(workstation.router)
 app.include_router(city.router)
