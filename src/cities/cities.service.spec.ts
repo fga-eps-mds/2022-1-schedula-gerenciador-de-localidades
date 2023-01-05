e89a59aadd5e4515c360b7368e95f227d3876f9c
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-01-05 17:43:23 -0300

    tests: add citites tests

--- src/cities/cities.service.spec.ts
+++ src/cities/cities.service.spec.ts
@@ -1,17 +1,29 @@
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CitiesController } from './cities.controller';
+import { DeleteResult, Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 import { CitiesService } from './cities.service';
 import { City } from './city.entity';
 import { CreateCityDto } from './dto/createCitydto';
+import { UpdateCityDto } from './dto/updateCitydto';
 
 describe('CitiesService', () => {
   let citiesService: CitiesService;
   let citiesRepository: Repository<City>;
 
+  const mockUuid = uuid();
+
   const mockCityDto: CreateCityDto = {
     name: 'Test Name',
+    state: 'DF',
+  };
+
+  const mockUpdateCityDto: UpdateCityDto = {
+    name: 'Test Name Updated',
     state: 'DF',
   };
 
@@ -26,13 +38,13 @@ describe('CitiesService', () => {
           useValue: {
             create: jest.fn().mockResolvedValue(new City()),
             find: jest.fn().mockResolvedValue(citiesEntityList),
-            findOne: jest.fn().mockResolvedValue(citiesEntityList[0]),
+            findOneBy: jest.fn().mockResolvedValue(citiesEntityList[0]),
             update: jest.fn(),
             delete: jest.fn(),
+            save: jest.fn(),
           },
         },
       ],
-      controllers: [CitiesController],
     }).compile();
 
     citiesService = module.get<CitiesService>(CitiesService);
@@ -42,5 +54,100 @@ describe('CitiesService', () => {
   it('should be defined', () => {
     expect(citiesService).toBeDefined();
     expect(citiesRepository).toBeDefined();
+  });
+
+  describe('createCity', () => {
+    const dto = mockCityDto;
+    it('should call city repository with correct params', async () => {
+      await citiesService.createCity(dto);
+      expect(citiesRepository.create).toHaveBeenCalledWith({
+        ...dto,
+      });
+      expect(citiesRepository.create);
+    });
+
+    it('should return an internal error exception', async () => {
+      jest.spyOn(citiesRepository, 'save').mockRejectedValue(new Error());
+
+      expect(citiesService.createCity(mockCityDto)).rejects.toThrowError(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('findCities', () => {
+    it('should return a city entity list successfully', async () => {
+      const response = await citiesService.findCities();
+
+      expect(response).toEqual(citiesEntityList);
+      expect(citiesRepository.find).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw a not found exception', () => {
+      jest.spyOn(citiesRepository, 'find').mockResolvedValueOnce([]);
+
+      expect(citiesService.findCities()).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('findCityById', () => {
+    const id = mockUuid;
+
+    it('should return an city entity successfully', async () => {
+      const response = await citiesService.findCityById(id);
+
+      expect(response).toEqual(citiesEntityList[0]);
+    });
+
+    it('should throw a not found exception', () => {
+      jest.spyOn(citiesRepository, 'findOneBy').mockResolvedValueOnce(null);
+
+      expect(citiesService.findCityById(id)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('updateCity', () => {
+    const id = mockUuid;
+    const dto = mockUpdateCityDto;
+
+    it('should return an updated city successfully', async () => {
+      const response = await citiesService.updateCity({ ...dto }, id);
+      expect(response).toMatchObject({ ...mockUpdateCityDto });
+    });
+
+    it('should return an internal server error exception when city cannot be updated', async () => {
+      jest.spyOn(citiesRepository, 'save').mockRejectedValue(new Error());
+
+      expect(citiesService.updateCity({ ...dto }, id)).rejects.toThrowError(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('deleteCity', () => {
+    it('should return a not found exception', () => {
+      const id = mockUuid;
+
+      jest.spyOn(citiesRepository, 'delete').mockResolvedValue(null);
+      expect(citiesService.deleteCity(id)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+
+    it('should return a not found exception', () => {
+      const id = mockUuid;
+
+      jest
+        .spyOn(citiesRepository, 'delete')
+        .mockResolvedValue({ affected: 0 } as DeleteResult);
+
+      expect(citiesService.deleteCity(id)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
   });
 });
